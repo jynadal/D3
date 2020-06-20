bed04f86616af2b532c65d1b8c3014e0cb4caedb
jynadal <nadaljeanyves@hotmail.fr>
2020-06-20 19:53:46 +0200

    Beggin BarChar Responsive

--- HandUpForD3/circle-d3/src/App.js
+++ HandUpForD3/circle-d3/src/App.js
@@ -1,75 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { select, axisBottom, axisLeft, scaleLinear, scaleBand } from 'd3';
 import './App.css';
+import BarChart from './BarChart';
 
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75,])
-  const svgRef = useRef();
-  console.log(svgRef);
-
-  // will be called initially and on every data changz
-  useEffect(() => {
-    const svg = select(svgRef.current);
-    const xScale = scaleBand()
-          .domain(data.map((value, index) => index))
-          .range([0, 300])
-          .padding(0.5);
-
-    const yScale = scaleLinear()
-          .domain([0, 150])
-          .range([150, 0]);
-    const colorScale = scaleLinear()
-          .domain([75,100, 150])
-          .range(["green","orange", "red"])
-          .clamp(true)
-
-    const xAxis = axisBottom(xScale).ticks(data.length);
-    svg
-        .select(".x-axis")
-        .style("transform", "translateY(150px)")
-        .call(xAxis);
-
-    const yAxis = axisLeft(yScale);
-    svg
-      .select(".y-axis")
-      .style("transform", "translateX(0px)")
-      .call(yAxis);
-
-    svg.selectAll(".bar")
-      .data(data)
-      .join("rect")
-      .attr("class", "bar")      
-      .style("transform", "scale(1, -1)")
-      .attr("x", (value, index) => xScale(index))
-      .attr("y", -150)      
-      .attr("width", xScale.bandwidth())
-      .on("mouseenter", (value, index) => {
-        svg
-          .selectAll(".tooltip")
-          .data([value])
-          .join(enter => enter.append("text").attr("y", yScale(value) - 4))
-          .attr("class", "tooltip")
-          .text(value)
-          .attr("x", xScale(index) + xScale.bandwidth() / 2)
-          .attr("text-anchor", "middle")
-          .transition()
-          .attr("y", yScale(value) - 8)
-          .attr("opacity", 1);
-      })
-      .on("mouseleave", () => svg.select(".tooltip").remove())
-      .transition()
-      .attr("fill", colorScale)
-      .attr("height", value => 150 - yScale(value));  
-   }, [data]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75,]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef}>
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </svg>
-      <br />
+      <BarChart data={data} />
+        <br />
       <button onClick={() => setData(data.map(value => value + 5))}>
         Update Data
       </button>
---
+++ HandUpForD3/circle-d3/src/BarChart.js
@@ -0,0 +1,82 @@
+import React, { useRef, useEffect } from 'react';
+import { select, axisBottom, axisLeft, scaleLinear, scaleBand } from 'd3';
+import './App.css';
+//import data from 'App';
+
+
+function BarChart({data}) {
+ // const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75,])
+  const svgRef = useRef();
+
+  // will be called initially and on every data changz
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    // scales
+    const xScale = scaleBand()
+          .domain(data.map((value, index) => index))
+          .range([0, 300])
+          .padding(0.5);
+
+    const yScale = scaleLinear()
+          .domain([0, 150])
+          .range([150, 0]);
+
+    const colorScale = scaleLinear()
+          .domain([75,100, 150])
+          .range(["green","orange", "red"])
+          .clamp(true)
+
+    // create x-axis
+    const xAxis = axisBottom(xScale).ticks(data.length);
+    svg
+        .select(".x-axis")
+        .style("transform", "translateY(150px)")
+        .call(xAxis);
+
+    // create y-axis
+    const yAxis = axisLeft(yScale);
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(0px)")
+      .call(yAxis);
+
+    svg.selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")      
+      .style("transform", "scale(1, -1)")
+      .attr("x", (value, index) => xScale(index))
+      .attr("y", -150)      
+      .attr("width", xScale.bandwidth())
+      .on("mouseenter", (value, index) => {
+        svg
+          .selectAll(".tooltip")
+          .data([value])
+          .join(enter => enter.append("text").attr("y", yScale(value) - 4))
+          .attr("class", "tooltip")
+          .text(value)
+          .attr("x", xScale(index) + xScale.bandwidth() / 2)
+          .attr("text-anchor", "middle")
+          .transition()
+          .attr("y", yScale(value) - 8)
+          .attr("opacity", 1);
+      })
+      .on("mouseleave", () => svg.select(".tooltip").remove())
+      .transition()
+      .attr("fill", colorScale)
+      .attr("height", value => 150 - yScale(value));  
+   }, [data]);
+
+  return (
+    <React.Fragment>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      
+      </React.Fragment>
+  );
+}
+
+export default BarChart;
